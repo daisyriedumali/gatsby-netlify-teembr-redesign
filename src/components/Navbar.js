3d461f767037d491dc2612b519d3fd8bf7cfbc59
eliebercero <elie.bercero@gmail.com>
2018-09-14 16:05:34 +0800

    Edits to markup and top navigation styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,37 +5,19 @@ import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
+  <div className="top-nav">
+    <div>
+      <h1>Teembr</h1>
+      <nav>
+        <a href="">Our Services</a>
+        <a href="">Case Studies</a>
+        <a href="">Testimonials</a>
+        <a href="">Blog</a>
+        <a href="">About Us</a>
+        <a href="" className="btn">Contact Us</a>
+      </nav>
     </div>
-  </nav>
+  </div>
 )
 
 export default Navbar
