b1ee8fb6b7e6236e255a4132bcdd16063b379a54
eliebercero <elie.bercero@gmail.com>
2018-10-03 14:14:44 +0800

    Added mobile menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,27 @@
 import React from 'react'
-import Link from 'gatsby-link'
-
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = () => (
-  <div className="top-nav">
-    <div>
+  <div>
+    <div className="top-nav">
+      <div>
+        <img src="img/svg/burger-menu.svg" className="burger-menu" />
+        <h1>
+          <a href="/">
+            <img src="img/svg/teembr-logo.svg" />
+          </a>
+        </h1>
+        <nav>
+          <a href="">Our Services</a>
+          <a href="">Case Studies</a>
+          <a href="">Testimonials</a>
+          <a href="">Blog</a>
+          <a href="/about-us">About Us</a>
+          <a href="" className="btn">Contact Us</a>
+        </nav>
+      </div>
+    </div>
+    <div className="mobile-menu">
+      <img src="img/svg/close-button.svg" className="close-button" />
       <h1>
         <a href="/">
           <img src="img/svg/teembr-logo.svg" />
@@ -24,4 +39,13 @@ const Navbar = () => (
   </div>
 )
 
-export default Navbar
+export default Navbar;
+
+// document.querySelector('.burger-menu').addEventListener('click', function(e) {
+//     let el = document.querySelector('.mobile-menu');
+//     el.classList.add('show');
+// });
+// document.querySelector('.close-button').addEventListener('click', function(e) {
+//     let el = document.querySelector('.mobile-menu');
+//     el.classList.remove('show');
+// });
