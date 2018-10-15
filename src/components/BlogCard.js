b088b1c25ce96258c8b40df88d2902aecd286bbe
Daisyrie Dumali <daisyrie.dumali@goabroad.com>
2018-10-15 15:14:56 +0800

    updated blog page and cloudinary images

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import imagePathHelper from '../components/helper/imagePathHelper'
 
 const BlogCard = ({ iconfill, data }) => {
     return (
         <div className="blog-card">
-            <img src={"/img/blogs/" + data.image} />
+            <img src={imagePathHelper(data.image)} />
             <div>
                 {data.claps}
                 <h2>{data.title}</h2>
                 <p>{data.snippet}</p>
                 <div className="author-info">
-                    <img src={"/img/blogs/" + data.headshot} />
+                    <img src={imagePathHelper(data.headshot)} />
                     <div>
                         <a href={data.link}>{data.author}</a>
                         <span>{data.date}</span>
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,76 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
-import Blogs from '../components/Blogs';
+import BlogCard from '../components/BlogCard'
 
-export default class BlogPage extends React.Component {
+export default class Blog extends React.Component {
   render() {
+    const { data } = this.props
+    const { edges: posts } = data.allMarkdownRemark
+
     return (
-        <section>
-          <Blogs />
-        </section>
+      <section>
+        <div className="blog">
+            <div className="blog-nav">
+                <nav>
+                    <a href="/">Design Resources</a>
+                    <a href="/">Articles</a>
+                </nav>
+            </div>
+            <div className="blog-cards-wrapper">
+                <div className="blog-cards">
+                    {posts
+                      .map(({ node: post }) => (
+                        <BlogCard 
+                          data={{
+                            image    : post.frontmatter.image,
+                            title    : post.frontmatter.title,
+                            snippet  : post.excerpt,
+                            headshot : post.frontmatter.authorImage,
+                            author   : post.frontmatter.authorName,
+                            link     : post.fields.slug,
+                            date     : post.frontmatter.date
+                          }}/>
+                    ))}
+                </div>
+            </div>
+        </div>        
+      </section>
     )
   }
 }
+
+Blog.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.array,
+    }),
+  }),
+}
+
+export const pageQuery = graphql`
+  query IndexQuery2 {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] },
+      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
+    ) {
+      edges {
+        node {
+          excerpt(pruneLength: 400)
+          id
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            templateKey
+            date(formatString: "MMMM DD, YYYY")
+            image
+            authorImage
+            authorName
+          }
+        }
+      }
+    }
+  }
+`
